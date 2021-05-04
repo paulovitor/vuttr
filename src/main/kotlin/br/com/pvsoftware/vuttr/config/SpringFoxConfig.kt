4ffeac2a51b8a7ffbab88cf40ff9c79b463e00b7
Paulo Vitor <paulovitor@MacBook-Pro-de-Paulo.local>
2021-05-04 09:44:34 -0300

    :bug: Fixing check bug

--- src/main/kotlin/br/com/pvsoftware/vuttr/config/SpringFoxConfig.kt
+++ src/main/kotlin/br/com/pvsoftware/vuttr/config/SpringFoxConfig.kt
@@ -13,7 +13,6 @@ import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.net.URI
 
-
 @Configuration
 @EnableSwagger2
 class SpringFoxConfig {
